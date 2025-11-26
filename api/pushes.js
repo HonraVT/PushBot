23c6e5d1a4d94785da5efc0ea7a5fe66fc09d2c9
HonraVT <111161607+HonraVT@users.noreply.github.com>
2025-11-26 13:05:46 -0300

    Update pushes.js

--- api/pushes.js
+++ api/pushes.js
@@ -1,12 +1,9 @@
-import { kv } from "@vercel/kv";
+import { get } from "@vercel/edge-config";
 
 export async function GET() {
-  const logs = await kv.lrange("push_logs", 0, -1);
+  const logs = (await get("push_logs")) || [];
 
-  return new Response(
-    JSON.stringify({ logs }),
-    {
-      headers: { "Content-Type": "application/json" }
-    }
-  );
+  return new Response(JSON.stringify({ logs }), {
+    headers: { "Content-Type": "application/json" }
+  });
 }
