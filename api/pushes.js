4e4dd9056b6ba2e29af0c07c7365a584bdc7b047
HonraVT <111161607+HonraVT@users.noreply.github.com>
2025-11-26 13:58:18 -0300

    Update pushes.js

--- api/pushes.js
+++ api/pushes.js
@@ -1,18 +1,26 @@
 import { createClient } from "redis";
 
-const client = createClient({
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
-  },
-  password: process.env.REDIS_PASSWORD || undefined
-});
+let client;
 
-client.connect();
+async function getClient() {
+  if (!client) {
+    client = createClient({
+      socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+      },
+      password: process.env.REDIS_PASSWORD || undefined,
+    });
+    await client.connect();
+  }
+  return client;
+}
 
 export default async function handler(req, res) {
+  const client = await getClient();
   const items = await client.lRange("push_logs", 0, -1);
   const logs = items.map(i => JSON.parse(i));
 
   res.status(200).json({ logs });
 }
+
