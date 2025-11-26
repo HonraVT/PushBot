fc73e27b6b13e6121388cd948d554ecc7254ffc4
HonraVT <111161607+HonraVT@users.noreply.github.com>
2025-11-26 13:09:26 -0300

    Update pushes.js

--- api/pushes.js
+++ api/pushes.js
@@ -1,9 +1,18 @@
-import { get } from "@vercel/edge-config";
+import { createClient } from "redis";
 
-export async function GET() {
-  const logs = (await get("push_logs")) || [];
+const client = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  },
+  password: process.env.REDIS_PASSWORD || undefined
+});
 
-  return new Response(JSON.stringify({ logs }), {
-    headers: { "Content-Type": "application/json" }
-  });
+client.connect();
+
+export default async function handler(req, res) {
+  const items = await client.lRange("push_logs", 0, -1);
+  const logs = items.map(i => JSON.parse(i));
+
+  res.status(200).json({ logs });
 }
