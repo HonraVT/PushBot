0d6510c3c66e473ab8e55cc805b83a5059c0fa37
HonraVT <111161607+HonraVT@users.noreply.github.com>
2025-11-26 13:50:13 -0300

    Update push.js

--- api/push.js
+++ api/push.js
@@ -2,16 +2,12 @@ import Redis from "ioredis";
 
 export default async function handler(req, res) {
   try {
-    // leitura do body no Node.js da Vercel
     let body = "";
     for await (const chunk of req) body += chunk;
 
-    const redis = new Redis(process.env.REDIS_URL, {
-      tls: {} // necessário para RedisLabs
-    });
+    const redis = new Redis(process.env.REDIS_URL); // sem TLS
 
     const key = `log:${Date.now()}`;
-
     await redis.set(key, body);
 
     redis.quit();
